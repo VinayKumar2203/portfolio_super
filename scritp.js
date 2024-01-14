dd31325e4c6a0b0114a4a0d8e28283f371bd61d4
Vinay Kumar <vinaysikhera2203@gmail.com>
2024-01-15 02:52:37 +0530

    navbar is ramaning

--- scritp.js
+++ scritp.js
@@ -34,3 +34,104 @@ let changeText = () => {
 
 changeText();
 setInterval(changeText, 3000);
+
+
+
+// circle skills/////////////////////
+
+const circles =document.querySelectorAll(".circle");
+circles.forEach(elem=>{
+    let dots=elem.getAttribute("data-dots");
+    let marked= elem.getAttribute("data-percent")
+    let percent=Math.floor(dots*marked/100);
+    let points="";
+    let rotate =360/dots
+
+    for (let i = 0; i < dots; i++) {
+        points+=`
+        <div class="points" style="--i:${i}; --rot:${rotate}deg"></div>
+        `;
+    }
+    elem.innerHTML=points;
+
+    const pointsMarked=elem.querySelectorAll(".points");
+    for (let i = 0; i < percent; i++) {
+       pointsMarked[i].classList.add("marked") 
+    }
+})
+
+
+// mix if up portfolio section 
+var mixer=mixitup('.portfolio-gallery');
+
+
+// active menu//////////////////////////////////
+
+let menuLi=document.querySelectorAll(".header ul li a")
+let section =document.querySelectorAll('section');
+
+function activeMenu() {
+    let len =section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+};
+
+activeMenu();
+window.addEventListener("scroll",activeMenu)
+
+// sticky navbar//////////////////////////////////
+
+const header=document.querySelector("header");
+window.addEventListener("scroll",function () {
+    header.classList.toggle("sticky",window.scrollY > 50)
+})
+
+
+//  tooggle icon navbar //////////////////////////////////
+let menuIcon=document.querySelector("#menu-icon");
+let navlist=document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+window.onscroll=()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
+// document.addEventListener('DOMContentLoaded', function () {
+//     const menuIcon = document.getElementById('menu-icon');
+//     const navList = document.querySelector('.navlist');
+
+//     menuIcon.addEventListener('click', function () {
+//         menuIcon.classList.toggle("bx-x");
+//         navList.classList.toggle('open');
+//     });
+// });
+
+
+
+
+
+
+const observer= new IntersectionObserver(()=>{
+    entries.forEach((entry)=>{
+      if(entry.inIresecting){
+        entry.target.classList.add("show-items");
+      }else{
+        entry.target.classList.remove("show-items");
+
+      }
+    });
+});
+
+
+const scrollScale =document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer(el));
+
+const scrollBotton =document.querySelectorAll(".scroll-botton");
+scrollBotton.forEach((el)=>observer(el));
+
+const scrollTop =document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer(el));
